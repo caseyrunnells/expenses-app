2a7e99674ae405bb09397eca70bb53325a5fb246
Casey Runnells <crunnells@gmail.com>
2018-03-12 00:20:32 -0500

    fixed redux thunk issue in configure store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
@@ -7,17 +7,14 @@ import authReducer from '../reducers/auth';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
-    const store = createStore(
-        combineReducers({
-            expenses: expensesReducer,
-            filters: filtersReducer,
-            auth: authReducer
-        }),
-        composeEnhancers(applyMiddleware(thunk))
-        //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+      auth: authReducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
-    return store;
+  return store;
 };
-
-
